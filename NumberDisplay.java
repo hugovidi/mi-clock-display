86e106b176cd31cb2d6fc55f313f04b6ed21ac9a
Hugo <hugo_vidinhas@hotmail.com>
2014-11-12 13:21:03 +0100

    añadidos todos los metodos, set para cambiar valor display, y los dos get para devolver el valor en string y int

--- NumberDisplay.java
+++ NumberDisplay.java
@@ -8,26 +8,67 @@
 public class NumberDisplay
 {
     // instance variables - replace the example below with your own
-    private int x;
-
+    private int display;
+    private int totalLimitDisplay;
+    
+    
     /**
-     * Constructor for objects of class NumberDisplay
+     * Constructor al cual se le itnroduce el limite del display y hace que el display se ponga a 0
      */
-    public NumberDisplay()
+    public NumberDisplay(int limitDisplay)
     {
-        // initialise instance variables
-        x = 0;
+      display = 0;
+      totalLimitDisplay = limitDisplay;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   /**
+    * Metodo setter que hace que el valor display se modifique por el introducido
+    */
+   public void setValue(int actualDisplay)
+   {
+       display = actualDisplay;
+       
+   }
+   
+   /**
+    * Método que devuelve el valor actual del display en forma de cadena de 2 caracteres
+    */
+   public String getDisplayValue()
+   {
+       String stringDisplayValue;   
+            if(display < 10 & display >= 0)
+       {
+           stringDisplayValue = "0" + display ;
+       }
+       else
+       {
+           stringDisplayValue = "" + display ;
+       }
+       return stringDisplayValue;   
+   }
+   
+   /**
+    * Método que devuelve el valor actual del display como entero 
+    */
+   public int getValue()
+   {
+       int intDisplay;
+       intDisplay = display;
+       return intDisplay; 
+   }
+   
+   /**
+    * Método que incrementa en 1 al display y lo hace volver al principio si alcanza el límite
+    */
+   public void increment()
+   {
+       if(display < totalLimitDisplay)
+       {
+           display+=1;
+       }
+       else
+       {
+        display = 0; 
+       }
+   }
 }
