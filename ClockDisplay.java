309f5d8f0f2e60fab1b937b49f517b197c653710
Hugo <hugo_vidinhas@hotmail.com>
2014-11-17 09:02:02 +0100

    añadida la clase ClockDisplay y sus dos constructores, el primero por defecto y el segundo a insertar

---
+++ ClockDisplay.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    // instance variables - replace the example below with your own
+    private NumberDisplay minuto;
+    private NumberDisplay hora;
+    private String horaActual;
+    
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+        // initialise instance variables
+        horaActual = "00:00";
+    }
+
+    
+      /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay(int minuto, int hora)
+    {
+        // initialise instance variables
+        
+    }
+}
+    
+   
